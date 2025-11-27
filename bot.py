6f0181fd07e6e4d50d0f6c18a17c433373db927f
XNeedNoName <xneednoname@tutamail.com>
2025-11-27 21:13:56 +0100

    Refactor file handling and update Chef method

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import sys, json, random, chefint, os
-
+import sys, json, random, chefint
+print("inited", flush=True, file=sys.stderr)
 random.seed(1)
 first_tick = True
 
@@ -10,8 +10,11 @@ for line in sys.stdin:
         config = data.get("config", {})
         width = config.get("width")
         height = config.get("height")
-    cheffile = os.open("bot.chef")
+        print("inited", flush=True, file=sys.stderr)
+    cheffile = open("bot.chef")
+    print(cheffile.read(), flush=True, file=sys.stderr)
     c = chefint.Chef(cheffile.read())
-    output = c.parse()
+    output = c.cook()
+    print(output, flush=True, file=sys.stderr)
     print(output, flush=True)
     first_tick = False
